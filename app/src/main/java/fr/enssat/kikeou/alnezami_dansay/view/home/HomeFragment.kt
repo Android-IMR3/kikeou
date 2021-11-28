ee0a130cc01ca1b79152c457486b34c77d39319e
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-11-28 15:03:50 +0100

    ✨ update my agenda

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/home/HomeFragment.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/home/HomeFragment.kt
@@ -5,20 +5,97 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.squareup.picasso.Picasso
+import fr.enssat.kikeou.alnezami_dansay.R
 import fr.enssat.kikeou.alnezami_dansay.databinding.FragmentHomeBinding
+import fr.enssat.kikeou.alnezami_dansay.model.entity.Agenda
+import kotlinx.coroutines.InternalCoroutinesApi
+import android.graphics.BitmapFactory
+
+import android.graphics.Bitmap
+
+
+
 
 
 class HomeFragment : Fragment() {
-
+    @InternalCoroutinesApi
+    private lateinit var homeViewModel: HomeViewModel
 
     private lateinit var binding: FragmentHomeBinding
 
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel.myAcount.observe(viewLifecycleOwner, Observer{agenda ->
+            binding(agenda)
+            val btnUpdate = binding.btnUpdateProfil
+            btnUpdate.setOnClickListener{
+                // Toast.makeText(context, "Update", Toast.LENGTH_SHORT).show()
+                //  findNavController().navigate(R.id.action_homeFragment_to_updateFragment)
+                val action = agenda?.let {
+                    HomeFragmentDirections.actionHomeFragmentToUpdateFragment(
+                        it
+                    )
+                }
+                action?.let { findNavController().navigate(it) }
+
+            }
+        })
+
+
+
+
+            return binding.root
+    }
+    fun binding(a: Agenda){
+        binding.nameUser.setText(a.name);
+        binding.emailUser.setText(a.contact.mail)
+        binding.phoneUser.setText(a.contact.tel)
+        binding.fbUser.setText(a.contact.fb)
+        Picasso.get().load(a.photo).into(binding.appBarImage)
+      /*  var bitmap = BitmapFactory.decodeFile(setImageByStatus(a.loc.day1))
+        binding.day1.setTag(1,a.loc.day1)
+        binding.day1.setImageBitmap(bitmap)
+        bitmap = BitmapFactory.decodeFile(setImageByStatus(a.loc.day2))
+        binding.day1.setTag(1,a.loc.day2)
+        binding.day2.setImageBitmap(bitmap)
+        bitmap = BitmapFactory.decodeFile(setImageByStatus(a.loc.day3))
+        binding.day1.setTag(1,a.loc.day3)
+        binding.day3.setImageBitmap(bitmap)
+        bitmap = BitmapFactory.decodeFile(setImageByStatus(a.loc.day4))
+        binding.day1.setTag(1,a.loc.day4)
+        binding.day4.setImageBitmap(bitmap)
+        bitmap = BitmapFactory.decodeFile(setImageByStatus(a.loc.day5))
+        binding.day1.setTag(1,a.loc.day5)
+        binding.day5.setImageBitmap(bitmap)*/
+    }
+    fun setImageByStatus(location: String):String{
+        var path=""
+        when (location) {
+            "off" -> {
+                    path= "src/main/res/mipmap-xhdpi/icon11.png"
+            }
+            "home" -> {
+                path= "src/main/res/mipmap-xhdpi/icon14.png"
+            }
+            "work" -> {
+                path= "src/main/res/mipmap-xhdpi/icon13.png"
+
+            }
+            else -> {
+                path= "src/main/res/mipmap-xhdpi/icon13.png"
+            }
+        }
+
+        return  path
     }
 }
--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/home/UpdateFragment.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/home/UpdateFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import fr.enssat.kikeou.alnezami_dansay.R
-import fr.enssat.kikeou.alnezami_dansay.databinding.FragmentRegisterBinding
+
 import fr.enssat.kikeou.alnezami_dansay.databinding.FragmentUpdateBinding
+import fr.enssat.kikeou.alnezami_dansay.model.entity.Agenda
+import fr.enssat.kikeou.alnezami_dansay.model.entity.Contact
+import fr.enssat.kikeou.alnezami_dansay.model.entity.Location
+import fr.enssat.kikeou.alnezami_dansay.view.readQR.FormNewContactViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
 
 
 /**
@@ -17,20 +26,68 @@ import fr.enssat.kikeou.alnezami_dansay.databinding.FragmentUpdateBinding
  */
 class UpdateFragment : Fragment() {
 
+    private val args: UpdateFragmentArgs by navArgs()
 
-
+    @InternalCoroutinesApi
+    private lateinit var updateViewModel: UpdateViewModel
     private lateinit var binding: FragmentUpdateBinding
 
 
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        updateViewModel = ViewModelProvider(this).get(UpdateViewModel::class.java)
 
+        val myprofile= args.myProfile
+        binding(myprofile)
+        val btnUpdate = binding.btnUpdateProfile
+        btnUpdate.setOnClickListener {
+            updateViewModel.updateMyProfile(getAgenda())
+           findNavController().navigate(R.id.action_updateFragment_to_homeFragment)
+        }
         return binding.root
     }
+    fun  binding(a: Agenda){
+        binding.nameUser.setText(a.name);
+        binding.emailUser.setText(a.contact.mail)
+        binding.phoneUser.setText(a.contact.tel)
+        binding.fbUser.setText(a.contact.fb)
+        binding.weekUser.setText(a.week.toString())
+        binding.photoUser.setText(a.photo)
+    }
+    fun getAgenda():Agenda{
+        val name = binding.nameUser.text.toString();
+        val week1 = binding.weekUser.text.toString()
+        var week:Int
+        week=1
+        try{
+            week =  week1.toInt();
+        }catch(e: Exception) {
+
+        }
+        var photo = binding.photoUser.text.toString();
+        if(photo.isEmpty()){
+            photo = "https://source.unsplash.com/1600x900/?avatar,person"
+        }
+        val email = binding.emailUser.text.toString();
+        val phone = binding.phoneUser.text.toString();
+        val fb = binding.fbUser.text.toString()
+
+
+        val loc = Location("day1","day2","day3","day4","day5")
+       // val loc = binding.locUser.text.toString()
+        return Agenda(0,name,photo,week,args.myProfile.loc, Contact(email,phone,fb))
+
+    }
+    fun getStatusByImage(img :String):String{
+        return ""
+    }
+
+
 
 
 }
