01fbbaea6e35813d9bc2b2f69e7bdcd0843625a1
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-11-28 15:00:15 +0100

    🔨 repository updates

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/repository/AgendaRepository.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/repository/AgendaRepository.kt
@@ -1,15 +1,23 @@
 package fr.enssat.kikeou.alnezami_dansay.model.repository
 
-import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
 import fr.enssat.kikeou.alnezami_dansay.model.dao.AgendaDao
 import fr.enssat.kikeou.alnezami_dansay.model.entity.Agenda
 
 class AgendaRepository(private val dao: AgendaDao) {
-    val allAgendas = dao.getAlphabetizedAgendas()
-    val myacount = dao.getMyaccount()
-    // call on a non-UI thread
-    @WorkerThread
+    val allAgendas: LiveData<List<Agenda>> = dao.getAlphabetizedAgendas()
+    val count : LiveData<Int> = dao.count()
+    val getMyaccount  = dao.getMyaccount()
+
+
     suspend fun insert(agenda: Agenda) {
         dao.insert(agenda)
     }
+    suspend fun update(agenda: Agenda) {
+        dao.update(agenda)
+    }
+    suspend fun delete(agenda: Agenda) {
+        dao.delete(agenda)
+    }
+
 }
