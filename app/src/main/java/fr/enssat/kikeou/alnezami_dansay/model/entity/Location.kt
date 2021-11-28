88d78336398b9b281debd9856efcd0811192d39b
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-11-28 14:59:50 +0100

    🔨 location constructor vide

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/entity/Location.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/entity/Location.kt
@@ -6,5 +6,6 @@ import kotlinx.android.parcel.Parcelize
 
 @JsonClass(generateAdapter = true)
 @Parcelize
-class Location(var day:Int, var place:String): Parcelable {
+class Location(var day1:String,var day2:String,var day3:String,var day4:String,var day5:String,): Parcelable {
+   constructor():this("","","","","")
 }
