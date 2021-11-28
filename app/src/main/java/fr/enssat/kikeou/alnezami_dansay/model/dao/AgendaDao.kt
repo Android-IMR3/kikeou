ed820436ffda7702e83b63fcb50cb7924a44edf1
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-11-28 14:58:36 +0100

    🔨  update dao

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/dao/AgendaDao.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/dao/AgendaDao.kt
@@ -1,8 +1,8 @@
 package fr.enssat.kikeou.alnezami_dansay.model.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import fr.enssat.kikeou.alnezami_dansay.model.entity.Agenda
-import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -12,12 +12,21 @@ interface AgendaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(agenda: Agenda)
 
+    @Update
+    fun update(agenda: Agenda)
+
+    @Delete
+    fun delete(agenda: Agenda)
+
     @Query("DELETE FROM agenda_table")
     fun deleteAll()
 
     @Query("SELECT * FROM agenda_table WHERE id=1")
-    fun getMyaccount(): Flow<Agenda>
+    fun getMyaccount(): LiveData<Agenda>
 
-    @Query("SELECT * FROM agenda_table ORDER BY name ASC")
-    fun getAlphabetizedAgendas(): Flow<List<Agenda>>
+    @Query("SELECT * FROM agenda_table WHERE id>1 ORDER BY name ASC")
+    fun getAlphabetizedAgendas(): LiveData<List<Agenda>>
+
+    @Query("SELECT Count() FROM agenda_table ")
+    fun count(): LiveData<Int>
 }
