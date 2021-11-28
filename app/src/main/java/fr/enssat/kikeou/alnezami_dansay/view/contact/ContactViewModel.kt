f5a594666ac74d1066b2f23f1c66572b799bb048
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-11-28 15:02:32 +0100

    🎨 refacor viewmodel

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/contact/ContactViewModel.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/contact/ContactViewModel.kt
@@ -4,16 +4,10 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import fr.enssat.kikeou.alnezami_dansay.model.KikeouDataBase
 import fr.enssat.kikeou.alnezami_dansay.model.repository.AgendaRepository
+import fr.enssat.kikeou.alnezami_dansay.view.UsabelViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @InternalCoroutinesApi
-class ContactViewModel  (application: Application) : AndroidViewModel(application) {
+class ContactViewModel  (application: Application) : UsabelViewModel(application) {
 
-    val respository: AgendaRepository
-
-
-    init {
-        val agendaDao = KikeouDataBase.getDatabase(application).agendaDao()
-        respository = AgendaRepository(agendaDao)
-    }
 }
