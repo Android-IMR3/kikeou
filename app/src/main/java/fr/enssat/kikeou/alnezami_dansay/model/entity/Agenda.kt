b64c2299bf1a5b6be7b441d76e287477d28d1a77
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-12-02 01:32:24 +0100

    refactor code

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/entity/Agenda.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/entity/Agenda.kt
@@ -1,38 +1,40 @@
 package fr.enssat.kikeou.alnezami_dansay.model.entity
 
 import android.os.Parcelable
-
-import androidx.room.TypeConverter
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 import kotlinx.parcelize.Parcelize
-import com.google.gson.reflect.TypeToken
-
-import com.google.gson.Gson
-import java.lang.reflect.Type
 
 
 @Parcelize
 @JsonClass(generateAdapter = true)
-data class LOC (
-    var day: Long,
-    var value: String
-): Parcelable{
-    constructor():this(1,Status.WORK.name)
+@Entity(tableName = "agenda_table")
+class Agenda(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    val name: String,
+    var photo: String,
+    val contact: List<Contact>,
+    val week: Long,
+    val loc: List<LOC>
+): Parcelable {
 
     public fun toJson(): String {
         val moshi = Moshi.Builder().build()
-        val adapter: JsonAdapter<LOC> = moshi.adapter(LOC::class.java)
+        val adapter: JsonAdapter<Agenda> = moshi.adapter(Agenda::class.java)
         return adapter.toJson(this)
     }
 
+
+
     companion object {
-        public fun fromJson(json: String): LOC? {
+        public fun fromJson(json: String): Agenda? {
             val moshi = Moshi.Builder().build()
-            val adapter: JsonAdapter<LOC> = moshi.adapter(LOC::class.java)
+            val adapter: JsonAdapter<Agenda> = moshi.adapter(Agenda::class.java)
             return adapter.fromJson(json)
         }
+
     }
 }
-
--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/entity/Contact.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/model/entity/Contact.kt
@@ -1,38 +1,31 @@
 package fr.enssat.kikeou.alnezami_dansay.model.entity
 
 import android.os.Parcelable
-
-import androidx.room.TypeConverter
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 import kotlinx.parcelize.Parcelize
-import com.google.gson.reflect.TypeToken
-
-import com.google.gson.Gson
-import java.lang.reflect.Type
 
 
 @Parcelize
 @JsonClass(generateAdapter = true)
-data class LOC (
-    var day: Long,
-    var value: String
-): Parcelable{
-    constructor():this(1,Status.WORK.name)
-
+class Contact (
+    val key: String,
+    val value: String
+): Parcelable {
+    constructor():this("", "")
     public fun toJson(): String {
         val moshi = Moshi.Builder().build()
-        val adapter: JsonAdapter<LOC> = moshi.adapter(LOC::class.java)
+        val adapter: JsonAdapter<Contact> = moshi.adapter(Contact::class.java)
         return adapter.toJson(this)
     }
 
+
     companion object {
-        public fun fromJson(json: String): LOC? {
+        public fun fromJson(json: String): Contact? {
             val moshi = Moshi.Builder().build()
-            val adapter: JsonAdapter<LOC> = moshi.adapter(LOC::class.java)
+            val adapter: JsonAdapter<Contact> = moshi.adapter(Contact::class.java)
             return adapter.fromJson(json)
         }
     }
 }
-
