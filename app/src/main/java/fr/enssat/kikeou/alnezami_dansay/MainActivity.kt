b33490b8ca581024a939e3de22f7fa7b4257f11b
ibrahimsyr95 <alnezami.sy@gmail.com>
2021-12-10 22:32:33 +0100

    🚧 in progresse searchbar

--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/MainActivity.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -29,37 +28,5 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController: NavController = navHostFragment.navController
         return navController.navigateUp() || super.onSupportNavigateUp()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menuInflater.inflate(R.menu.nav_menu,menu)
-        val search :MenuItem? = menu?.findItem(R.id.app_bar_search)
-        val searchView  : SearchView =search?.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-              //  Toast.makeText(context,"hey",Toast.LENGTH_SHORT).show()
-                return true
-            }
-        })
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        super.onOptionsItemSelected(item)
-        when (item?.itemId)
-        {
-            R.id.app_bar_search ->
-            {
-
-            }
-
-            else -> { Toast.makeText(this,"action inconnu",Toast.LENGTH_LONG).show() }
-        }
-        return true
     }
 }
--- app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/list/ListPersonFragment.kt
+++ app/src/main/java/fr/enssat/kikeou/alnezami_dansay/view/list/ListPersonFragment.kt
@@ -1,9 +1,10 @@
 package fr.enssat.kikeou.alnezami_dansay.view.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,8 +19,15 @@ import kotlinx.coroutines.InternalCoroutinesApi
 
 class ListPersonFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
-        @InternalCoroutinesApi
-        private lateinit var mPersonViewModel: ListPersonViewModel
+    @InternalCoroutinesApi
+    private lateinit var mPersonViewModel: ListPersonViewModel
+    private var adapter = PersonAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,14 +39,15 @@ class ListPersonFragment : Fragment() {
 
         val view = binding.root
         //recyclerview
-        var adapter = PersonAdapter()
+
         val recyclerview = binding.recyclerview
         recyclerview.adapter= adapter
         recyclerview.layoutManager = LinearLayoutManager(requireContext())
 
-         mPersonViewModel = ViewModelProvider(this).get(ListPersonViewModel::class.java)
+        mPersonViewModel = ViewModelProvider(this).get(ListPersonViewModel::class.java)
 
         mPersonViewModel.allAgendas.observe(viewLifecycleOwner, Observer{user -> adapter.setData(user)})
+        //if no data in data base send user to register his data
         mPersonViewModel.count.observe(viewLifecycleOwner, Observer{count -> if(count==0){
             findNavController().navigate(fr.enssat.kikeou.alnezami_dansay.R.id.action_listFragment_to_registerFragment)
         } })
@@ -60,5 +69,42 @@ class ListPersonFragment : Fragment() {
         }
        return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.nav_menu,menu)
+        val search : MenuItem? = menu?.findItem(R.id.app_bar_search)
+        val searchView  : SearchView =search?.actionView as SearchView
+        searchView.queryHint = "Search something"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter?.filter?.filter(newText)
+                if (newText != null) {
+                    Log.i("search fragment",newText)
+                }
+                return true
+
+            }
+        })
+        search.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                adapter?.filter?.filter("")
+
+                Toast.makeText(context,"Action Collapse",Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+
+                Toast.makeText(context,"Action Expand",Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu!!, inflater!!)
+    }
+
 
 }
